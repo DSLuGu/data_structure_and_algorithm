31d281a4d135dcf4ac7b76921918ccde1bdcf372
ds_lminho248 <ds.lminho248@gmail.com>
2021-02-02 19:21:36 +0900

    solve the problems of baejoon (1934, 9613)

---
+++ baekjoon/bj_9613.py
@@ -0,0 +1,22 @@
+import sys
+import itertools
+input = sys.stdin.readline
+
+def gcd(a, b):
+    return gcd(b, a % b) if b else a
+
+def main():
+    '''source: https://www.acmicpc.net/problem/9613'''
+    T = int(input())
+    for t in range(T):
+        num_list = list(map(int, input().split()))
+        num_list = num_list[1:]
+        ans = 0
+        for a, b in itertools.combinations(num_list, 2):
+            ans += gcd(a, b)
+        print(ans)
+    return None
+
+
+if __name__ == '__main__':
+    main()
